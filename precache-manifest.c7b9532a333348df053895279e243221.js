a67acebc7bc09a87d21c7fd6f8478ce1de860a1c
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-12 10:10:15 +0530

    Updates

---
+++ precache-manifest.c7b9532a333348df053895279e243221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b753beb9fc22811a09030c83b865bbaf",
+    "url": "/tourizto/index.html"
+  },
+  {
+    "revision": "630931b6399b0e1b0828",
+    "url": "/tourizto/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85e17be7fa8cba9f83d7",
+    "url": "/tourizto/static/js/2.377213c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tourizto/static/js/2.377213c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630931b6399b0e1b0828",
+    "url": "/tourizto/static/js/main.b8fb9888.chunk.js"
+  },
+  {
+    "revision": "e5f53c9b0e7967de0d47",
+    "url": "/tourizto/static/js/runtime-main.cf5ff57a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tourizto/static/media/logo.5d5d9eef.svg"
+  }
+]);
