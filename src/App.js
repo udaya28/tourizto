b4c526712b71c2345eb71904e14254f0a4426339
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-13 22:39:33 +0530

    added container2 and bg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header/header.component';
 import { Container1 } from './components/container1/container1.component';
+import { Container2 } from './components/container2/container2.component';
 function App() {
   // window.onscroll = function() {console.log(document.documentElement.scrollTop)};
   return (
     <div className="App">
       <Header />
       <Container1 />
+      <Container2/>
     </div>
   );
 }
---
+++ src/components/container2/container2.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './container2.styles.css';
+export const Container2 = () => {
+  return (
+    <div className="con2">
+      <h2>Why Travellino</h2>
+      
+    </div>
+  );
+};
