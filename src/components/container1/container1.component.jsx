54f6c80420bacdf77cff3ab696b85fad63ea6858
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-13 21:50:34 +0530

    added input box

--- src/components/container1/container1.component.jsx
+++ src/components/container1/container1.component.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './component1.styles.css'
 export  const Container1=()=> {
     return (
-        <div className="con1">
-            
+        <div className=" con1 ">
+            <h1>Enjoy Your Dream Vacation</h1>
+            <p>Travel to the any corner of the world, without going around in circles.</p>
+            <div className="input-search">
+            <input type="text" placeholder="Enter Keyword" />
+            <button >SEARCH</button>
+            </div>
         </div>
     )
 }
